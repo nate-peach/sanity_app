cd1a2583770eddd6bd9ff114951123dd7e842c16
nate-peach <nathanpeach90@gmail.com>
2022-07-21 12:45:35 -0700

    adding changes made during flight

--- server.js
+++ server.js
@@ -3,32 +3,35 @@ const app = express();
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 
-MongoClient.connect('mongodb+srv://natepeach:8786@sanity-app.nhg4sln.mongodb.net/?retryWrites=true&w=majority', 
-{useUnifiedTopology: true})
-.then(client => {
-  console.log('Connected to Database')
-  const db = client.db('sanity-quotes')
-  const quotesCollection = db.collection('quotes')
-})
-.catch(error => console.error(error))
-
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+  console.log('listening on 3000')
+})
 
 console.log('Node is the path to enlightenment')
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
-  })
+MongoClient.connect(
+  'mongodb+srv://natepeach:8786@sanity-app.nhg4sln.mongodb.net/?retryWrites=true&w=majority', 
+{useUnifiedTopology: true})
+.then((client) => {
+  console.log('Connected to Database')
+  const db = client.db('new-sanity-quote')
+  const quotesCollection = db.collection('quotes')
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
-    quotesCollection.insertOne(req.body)
-    .then(result => {
-      console.log(result)
-    })
-    .catch(error => console.error(error))
+app.get('/', (req, res) => {
+res.sendFile(__dirname + '/index.html')
+})
+
+  app.post('/quotes', (req, res) => {
+console.log(req.body)
+      quotesCollection
+          .insertOne(req.body)
+          .then((result) => {
+              console.log('this is result',result)
+    res.redirect('/')
+          })
+          .catch((error) => console.error(error))
   })
+})
+.catch((error) => console.error(error))
